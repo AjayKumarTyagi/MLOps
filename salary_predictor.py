486835457abd28a8879316d0aa0b8f7e092afebc
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-04-14 13:47:08 +0530

    added menu

--- salary_predictor.py
+++ salary_predictor.py
@@ -2,7 +2,16 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.externals import joblib
 model=joblib.load("salary_model.pk1")
-
-exp=float(input("Enter job experience: "))
-
-print("Expected salary: ",float(model.predict([[exp]])))
+print("\t\t\tWelcome to Future Tools")
+print("\t\t\t-----------------------\n")
+while True:
+    print("0. Exit program")
+    print("1. Predict salary based on experience")
+    choice=input("Enter your choice: ")
+    if choice=="1":
+        exp=float(input("Enter job experience: "))
+        print("Expected salary: ",float(model.predict([[exp]])))
+    elif choice=="0":
+        exit()
+    else:
+        print("invalid choice")
